6f6ad657564e78c12f41a73042e305e26c3cabd8
mouhadiol <122018731+mouhadiol@users.noreply.github.com>
2025-03-13 13:48:02 +0000

    projet python

---
+++ diol/articles/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Article(models.Model):
+    titre = models.CharField(max_length=100)
+    contenu = models.TextField()
+    date_creation = models.DateTimeField(auto_now_add=True)
+    date_mise_a_jour = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.titre
